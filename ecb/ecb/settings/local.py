bf43bb32d982faee3a9356b66a8fe4e255a576c4
Paweł Czarnecki <pawel.czarnecki@halodoktorze.pl>
2018-08-28 22:09:52 +0200

    Added configuration settings for Celery and Redis

--- ecb/ecb/settings/local.py
+++ ecb/ecb/settings/local.py
@@ -120,3 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Celery
+
+BROKER_URL = 'redis://localhost:6379'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_TIMEZONE = 'Europe/Warsaw'
